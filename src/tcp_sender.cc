e8da1940aa624ec7f7ce9f03331589999d897a5f
akane <1297228257@qq.com>
2025-03-25 21:34:51 +0800

    初步实现 push 和 receive 逻辑、计时器类 未实现 tick 与超时重传机制

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -2,13 +2,13 @@
 #include "debug.hh"
 #include "tcp_config.hh"
 
+
 using namespace std;
 
 // This function is for testing only; don't add extra state to support it.
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  debug( "unimplemented sequence_numbers_in_flight() called" );
-  return {};
+	return flight_count;
 }
 
 // This function is for testing only; don't add extra state to support it.
@@ -20,20 +20,85 @@ uint64_t TCPSender::consecutive_retransmissions() const
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  debug( "unimplemented push() called" );
-  (void)transmit;
+
+	TCPSenderMessage message;
+
+	// 检查 payload 长度
+	uint64_t segment_len = window_size - flight_count;
+	if (segment_len > TCPConfig::MAX_PAYLOAD_SIZE) {
+		segment_len = TCPConfig::MAX_PAYLOAD_SIZE;
+	}
+
+	// 头序号
+	message.seqno = head_seqno();
+
+	// SYN
+	if (SYN == false && reader().bytes_popped() == 0) {
+		message.SYN = true;
+		SYN = true;
+	}
+
+	// FIN
+	if (FIN == false && flight_count == 0 && reader().is_finished() == true) {
+		message.FIN = true;
+		FIN = true;
+	}
+
+	// payload
+	uint64_t payload_len = segment_len - message.SYN - message.FIN;
+
+	read(reader(), payload_len, message.payload);
+
+	// RST
+	if (writer().has_error() == true) {
+		message.RST = true;
+		RST = true;
+	}
+
+	// message 非空
+	if (message.sequence_length() > 0) {
+
+		transmit(message);
+	
+		// 将 message 存进数据结构，用于超时重传
+		
+		lost_messages.push(message);
+
+		flight_count += message.sequence_length();
+
+	}
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
-  debug( "unimplemented make_empty_message() called" );
-  return {};
+	TCPSenderMessage message;
+
+	message.seqno = head_seqno();		
+	
+	return message;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  debug( "unimplemented receive() called" );
-  (void)msg;
+	window_size = msg.window_size;
+
+	// 检查 ackno，移除已经收到的包
+	while (lost_messages.empty() == false && !(msg.ackno == lost_messages.front().seqno) ) {
+
+		flight_count -= lost_messages.front().sequence_length();
+		lost_messages.pop();
+
+		reTimer.reset_RTO();
+
+	}
+
+	// 检查是否仍有未收到的包，更新计时器
+	if (lost_messages.empty() == true) {
+		reTimer.stop();	
+	}
+	else {
+		reTimer.start();
+	}
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -6,12 +6,45 @@
 
 #include <functional>
 
+class RetransmissionTimer {
+
+public:
+	
+	RetransmissionTimer(uint64_t RTO_ms) : RTO_ms_(RTO_ms), initial_RTO_ms_(RTO_ms) {}
+
+	// 检查超时
+	bool overtime() { return is_started && timer >= RTO_ms_; }
+
+	// 检查启动状态
+	bool running() { return is_started; }
+
+	// 启动、关闭、更新时间
+	void start() { timer = 0; is_started = true; }
+	void stop() { timer = 0; is_started = false; }
+	void update( uint64_t ms_since_last_tick ) { timer += ms_since_last_tick; }
+	
+	// RTO 修改
+	void reset_RTO() { set_RTO_ms_(initial_RTO_ms_); }
+	void double_RTO() { set_RTO_ms_(RTO_ms_ * 2); }
+
+private:
+	
+	uint64_t RTO_ms_;	
+	uint64_t initial_RTO_ms_;
+	uint64_t timer { 0 };
+	bool is_started { false };
+
+	void set_RTO_ms_( uint64_t RTO_ms ) { RTO_ms_ = RTO_ms; }
+};
+
+
+
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), reTimer( initial_RTO_ms_ )
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -36,10 +69,25 @@ public:
   const Reader& reader() const { return input_.reader(); }
   Writer& writer() { return input_.writer(); }
 
+  Wrap32 head_seqno() const { return isn_ + reader().bytes_popped() + SYN; }
+
 private:
   Reader& reader() { return input_.reader(); }
 
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+
+  	uint16_t window_size { 1 };
+	bool SYN { false };
+	bool FIN { false };
+	bool RST { false };
+	
+  	uint64_t resend_count { 0 };
+	RetransmissionTimer reTimer;
+
+	uint64_t flight_count { 0 };
+
+	std::queue<TCPSenderMessage> lost_messages { };
 };
+
