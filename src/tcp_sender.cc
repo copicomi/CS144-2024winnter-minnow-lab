9dee5cacffcbecdba007b2f38c8266e369a4edd0
akane <1297228257@qq.com>
2025-03-26 00:16:30 +0800

    PASSED TEST #33 必须改成 绝对序列号 才能进行比较，否则会导致无限循环 另外需要处理ackno在msg中间的情况

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -2,6 +2,7 @@
 #include "debug.hh"
 #include "tcp_config.hh"
 
+#include <iostream>
 
 using namespace std;
 
@@ -20,55 +21,56 @@ uint64_t TCPSender::consecutive_retransmissions() const
 void TCPSender::push( const TransmitFunction& transmit )
 {
 
-	TCPSenderMessage message;
-
 	// 检查 payload 长度
-	uint64_t segment_len = window_size - flight_count;
-	if (segment_len > TCPConfig::MAX_PAYLOAD_SIZE) {
-		segment_len = TCPConfig::MAX_PAYLOAD_SIZE;
-	}
+	uint64_t total_segment_len = window_size - flight_count;
 
-	// 头序号
-	message.seqno = head_seqno();
+	for (uint64_t i = 0; i < total_segment_len; i += TCPConfig::MAX_PAYLOAD_SIZE) {
 
-	// SYN
-	if (SYN == false && reader().bytes_popped() == 0) {
-		message.SYN = true;
-		SYN = true;
-	}
-
-	// assume FIN followed
-	if (FIN == false && writer().is_closed() == true && message.SYN + reader().bytes_buffered() + 1 <= segment_len) {
-		message.FIN = true;
-		FIN = true;
-	}
-
-	// payload
-	uint64_t payload_len = segment_len - message.SYN - message.FIN;
-
-	read(reader(), payload_len, message.payload);
-
-	// RST
-	if (writer().has_error() == true) {
-		message.RST = true;
-		RST = true;
-	}
-
-	// message 非空
-	if (message.sequence_length() > 0) {
-
-		transmit(message);
-	
-		// 将 message 存进数据结构，用于超时重传
+		TCPSenderMessage message;
 		
-		lost_messages.push(message);
-
-		flight_count += message.sequence_length();
-
-		if (reTimer.running() == false) {
-			reTimer.start();
+		uint64_t segment_len = min(total_segment_len - i, TCPConfig::MAX_PAYLOAD_SIZE);
+		// 头序号
+		message.seqno = head_seqno();
+	
+		// SYN
+		if (SYN == false && reader().bytes_popped() == 0) {
+			message.SYN = true;
+			SYN = true;
 		}
+	
+		// assume FIN followed
+		if (FIN == false && writer().is_closed() == true && message.SYN + reader().bytes_buffered() + 1 <= segment_len) {
+			message.FIN = true;
+			FIN = true;
+		}
+	
+		// payload
+		uint64_t payload_len = segment_len - message.SYN - message.FIN;
+	
+		read(reader(), payload_len, message.payload);
+	
+		// RST
+		if (writer().has_error() == true) {
+			message.RST = true;
+			RST = true;
+		}
+	
+		// message 非空
+		if (message.sequence_length() > 0) {
+	
+			transmit(message);
+		
+			// 将 message 存进数据结构，用于超时重传
+			
+			lost_messages.push(message);
+	
+			flight_count += message.sequence_length();
+	
+			if (reTimer.running() == false) {
+				reTimer.start();
+			}
 
+		}
 	}
 }
 
@@ -86,6 +88,8 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 	// 更新窗口大小
 	window_size = msg.window_size;
 
+	bool received_new_data = false;
+
 	// 检查 ackno，移除已经收到的包
 	while ( lost_messages.empty() == false && msg.ackno > lost_messages.front().seqno && 
 		 									  msg.ackno <= lost_messages.front().seqno + flight_count) {
@@ -93,18 +97,19 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 		flight_count -= lost_messages.front().sequence_length();
 		lost_messages.pop();
 
-		reTimer.reset_RTO();
-
-		resend_count = 0;
+		received_new_data = true;
 
 	}
 
 	// 检查是否仍有未收到的包，更新计时器
+	if (received_new_data == true) {
+		reTimer.reset_RTO();
+		resend_count = 0;
+		reTimer.start();
+	}
+
 	if (lost_messages.empty() == true) {
 		reTimer.stop();	
-	}
-	else {
-		reTimer.start();
 	}
 }
 
@@ -115,7 +120,7 @@ void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& trans
 	
 	if (reTimer.overtime() == true) {
 		transmit(lost_messages.front());
-		// TODO : 如果窗口非空，还要进行操作
+
 		if (window_size > 0) {
 			
 			resend_count ++;
